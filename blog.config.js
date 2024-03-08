55093c5b0ed22a1b598f4eb205067003a3f8050d
Wang Peng <46642321+wangpeng5@users.noreply.github.com>
2024-03-08 14:00:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '且行且歌', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '分享技术与生活🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.wangpeng.run', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '年少追风,博客,追风一直跑 ,金融科技 , 银行核心系统' // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '年少追风','博客','追风一直跑','金融科技','银行核心系统', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'hello@wangpeng.run', // 邮箱地址 例如mail@tangly1024.com
