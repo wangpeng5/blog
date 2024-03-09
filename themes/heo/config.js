889bd2a4e060c3d3489835b0fd4dde0c3da873d2
Wang Peng <46642321+wangpeng5@users.noreply.github.com>
2024-03-09 11:39:47 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,11 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: '#' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: 'Welcome 欢迎光临🎯', url: '#' },
+    { title: '昨天是段历史☠️', url: '#' },
+    { title: '明天是个谜团👻', url: '#' },
+    { title: '今天是天赐的礼物☃️', url: '#' },
+    { title: '要像珍惜礼物一样珍惜今天❤️', url: '#' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -17,31 +20,31 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享技术',
   HEO_HERO_TITLE_2: '与美好生活',
-  HEO_HERO_TITLE_3: 'WANGPENG.RUN',
-  HEO_HERO_TITLE_4: '欢迎访问',
-  HEO_HERO_TITLE_5: '金融科技',
+  HEO_HERO_TITLE_3: '科技、金融、认知、效率、开发',
+  HEO_HERO_TITLE_4: 'wangpeng.run',
+  HEO_HERO_TITLE_5: '追风一直跑',
   HEO_HERO_TITLE_LINK: '#',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/推荐' },
+  HEO_HERO_CATEGORY_2: { title: '技术分享', url: '/category/技术分享' },
+  HEO_HERO_CATEGORY_3: { title: '金融科技', url: '/category/金融科技' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
-  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
+    '🏠 底层知识探索者',
+    '🤝 钻研技术与实践',
     '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/wangpeng5', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: '/about', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -104,10 +107,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '微信公众号',
+  HEO_SOCIAL_CARD_TITLE_2: '更多精彩内容等你发现',
+  HEO_SOCIAL_CARD_TITLE_3: '点击查看>>',
+  HEO_SOCIAL_CARD_URL: '/about',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
