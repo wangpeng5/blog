fb76e985b308fc6a92b2f886b9af7c643e34eea0
wangpeng <1106745064@qq.com>
2024-03-27 23:33:46 +0800

    个性化网站

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -125,7 +125,7 @@ const CONFIG = {
   HEO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
   HEO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  HEO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
+  HEO_ARTICLE_COPYRIGHT: false, // 显示文章版权声明
   HEO_ARTICLE_RECOMMEND: true, // 文章关联推荐
 
   HEO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
